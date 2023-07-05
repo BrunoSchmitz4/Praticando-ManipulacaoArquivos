df7c7f1f69e32b90239deefe62ddf1a2674121d7
Bruno Silva <brunosch250@gmail.com>
2023-07-05 10:21:32 -0300

    Incluindo using block

--- FirstFileProgram/Program.cs
+++ FirstFileProgram/Program.cs
@@ -9,46 +9,23 @@ namespace FirstFileProgram
     {
         static void Main(string[] args)
         {
-            // Criamos uma variável com um caminho para file1.txt
-            string path1 = @"C:\Users\OS0050\Documents\file1.txt";
-            // Então, criamos variáveis nulas, sendo uma 
-            // do tipo superclasse FileStream, a outra,
-            // do tipo superclasse StreamReader.
-            StreamReader sr = null;
 
-            // O try cria um objeto do tipo Filestream que
-            // recebe de parâmetro o path1 e instrução do que fazer.
-            // No caso, abrir file1.txt.
+            string path = @"C:\Users\OS0050\Documents\file1.txt";
 
-            // Aberto, sr cria um objeto StreamReader,
-            // recebendo os valores do filestream.
-            // Então, criasse uma variável que lê as linhas da
-            // variável streamreader com os dados de filestream.
-
-            // Obs: versão mais enxuta e prática
             try
             {
-                sr = File.OpenText(path1);
-                
-                while (!sr.EndOfStream)
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    string lines = sr.ReadLine();
-                    Console.WriteLine(lines);
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
                 }
             }
             catch (IOException ex)
             {
-                Console.WriteLine("An error accurred");
-                Console.WriteLine(ex.Message);
-            }
-            // Contudo, as streams são recursos externos, pra interagirmos
-            // com o sistema operacional. Acima pedimos pra abrir um arquivo.
-            // Este recurso não é gerenciado pelo CLR do .NET.
-            // Por isso, deve ser fechado manualmente com finally.
-            // Ex: forma explícita e funcional.
-            finally
-            {
-                if (sr != null) sr.Close();
+                Console.Write("An error occurred!: " + ex.Message);
             }
         }
     }
