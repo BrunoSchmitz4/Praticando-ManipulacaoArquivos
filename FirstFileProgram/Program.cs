b1e4a0c17f8c1bf2f4d3b7e9a1c320c8415ad94e
Bruno Silva <brunosch250@gmail.com>
2023-07-05 11:01:53 -0300

    Aprendendo streamwriter

--- FirstFileProgram/Program.cs
+++ FirstFileProgram/Program.cs
@@ -11,22 +11,41 @@ namespace FirstFileProgram
         {
 
             string path = @"C:\Users\OS0050\Documents\file1.txt";
+            string sourcePath = @"C:\Users\OS0050\Documents\file1.txt";
+            string targetPath = @"C:\Users\OS0050\Documents\file2.txt";
 
-            try
-            {
-                using (StreamReader sr = File.OpenText(path))
-                {
-                    while (!sr.EndOfStream)
-                    {
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
-                    }
-                }
-            }
-            catch (IOException ex)
-            {
-                Console.Write("An error occurred!: " + ex.Message);
-            }
+            //try
+            //{
+            //    string[] lines = File.ReadAllLines(sourcePath);
+
+            //    using (StreamWriter sw = File.AppendText(targetPath))
+            //    {
+            //        foreach (string line in lines)
+            //        {
+            //            sw.WriteLine(line.ToUpper());
+            //        }
+            //    }
+            //}catch (IOException ex)
+            //{
+            //    Console.WriteLine("An error occurred! " + ex.Message);
+            //}
+
+
+            //try
+            //{
+            //    using (StreamReader sr = File.OpenText(path))
+            //    {
+            //        while (!sr.EndOfStream)
+            //        {
+            //            string line = sr.ReadLine();
+            //            Console.WriteLine(line);
+            //        }
+            //    }
+            //}
+            //catch (IOException ex)
+            //{
+            //    Console.Write("An error occurred!: " + ex.Message);
+            //}
         }
     }
 }
